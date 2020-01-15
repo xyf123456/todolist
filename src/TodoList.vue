5f9bd0a7ea5e500c63cc02da9a05f75bb7097123
xuyunfei <905231404@qq.com>
2020-01-15 12:21:51 +0800

    使用vue cli脚手架开发了todolist组件

---
+++ src/TodoList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div>
+      <input class="item" v-model="inputValue">
+      <button @click="handleSubmit">提交</button>
+      <ul>
+        <todo-item
+          v-for="(item,index) of list"
+          :content="item"
+          :index="index"
+          :key="index"
+          @delete="handleDelete">
+        </todo-item>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+
+export default {
+  components: {
+    'todo-item': TodoItem
+  },
+  // 作为组件data不是一个属性了，而是一个函数
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit () {
+      // alert(123)
+      // this.$data.list.push(this.inputValue)
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete (index) {
+      // alert(index)
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li class="item" @click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+  // 子组件接受父组件传来的参数
+  props: ['content', 'index'],
+  methods: {
+    handleDelete () {
+      // 输出接受到父组件的传递的数据  向父组件触发一些事件
+      // alert(this.index)
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only ,
+scoped的作用：只有本身的组件的样式才能起作用，如果去掉的话则为全局样式-->
+<style scoped>
+  .item {
+    color: aqua;
+  }
+</style>
